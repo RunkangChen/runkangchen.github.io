92315b9af78fa06d4edb67317d2f487024434d40
informatic365 <112966168+RunkangChen@users.noreply.github.com>
2023-03-04 19:22:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function() {
 	// Set the amount of time the loading screen should be shown in milliseconds
-	var timeOut = 3000;
+	var timeOut = 5500;
 
 	// Hide the loading screen after the specified time has elapsed
 	setTimeout(function() {
