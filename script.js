678eacf622e551a0a17f83c762ceedaa65baa63b
informatic365 <112966168+RunkangChen@users.noreply.github.com>
2023-03-04 19:16:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const redirectUrl = "runkangchen.github.io/check/4.55.1/update/check";
-const delayInSeconds = 4;
 
-setTimeout(() => {
-  window.location.href = redirectUrl;
-}, delayInSeconds * 1000);
+
+window.addEventListener("load", function() {
+
+	// Set the amount of time the loading screen should be shown in milliseconds
+
+	var timeOut = 3000;
+
+	// Hide the loading screen after the specified time has elapsed
+
+	setTimeout(function() {
+
+		var loadingScreen = document.querySelector(".loading-screen");
+
+		loadingScreen.style.display = "none";
+
+	}, timeOut);
+
+});
