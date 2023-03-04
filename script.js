03f4cc341af75c274fb1722c05c9d7a913c42492
informatic365 <112966168+RunkangChen@users.noreply.github.com>
2023-03-04 19:18:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-
-
 window.addEventListener("load", function() {
-
 	// Set the amount of time the loading screen should be shown in milliseconds
-
 	var timeOut = 3000;
 
 	// Hide the loading screen after the specified time has elapsed
-
 	setTimeout(function() {
-
 		var loadingScreen = document.querySelector(".loading-screen");
-
 		loadingScreen.style.display = "none";
-
 	}, timeOut);
-
 });
